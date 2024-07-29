17c7c0c28c9625089b2512bfe09d83fd28a5766c
Miguel Ligero <miguel.ligero79@gmail.com>
2024-07-29 15:53:13 +0200

    Add spring-boot project files

---
+++ src/main/java/com/vayadrop/vayadrop/VayadropApplication.java
@@ -0,0 +1,13 @@
+package com.vayadrop.vayadrop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VayadropApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VayadropApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vayadrop/vayadrop/VayadropApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vayadrop.vayadrop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VayadropApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
